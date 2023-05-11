db464175fd7e3d87887f1e6a8ce9385cd461011a
NEYAZ N <neyaznafiz@outlok.com>
2023-05-12 01:00:09 +0600

    user interface and schema created for test user

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,89 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
+import { Schema } from "mongoose";
 
 const app: Application = express();
+// cors
 app.use(cors());
+// parse data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Practice server running..!");
+  // inserting a test data into mongodb
+
+  // interface
+  interface IUser {
+    id: string;
+    role: string;
+    password: string;
+    name: {
+      firstName: string;
+      lastName: string;
+    };
+    dateOfBirth: string;
+    gender: "male" | "female";
+    email?: string;
+    contactNo: string;
+    emergenctContactNo: string;
+    presentAddress: string;
+    permentAddress: string;
+  }
+
+  // schema
+  const userSchema = new Schema<IUser>({
+    id: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    role: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    name: {
+      firstName: {
+        type: String,
+        required: true,
+      },
+      lastName: {
+        type: String,
+        required: true,
+      },
+    },
+    dateOfBirth: {
+      type: String,
+    },
+      gender: {
+        type: String,
+        enum: ["male", "female"],
+    },
+    email: {
+      type: String,
+      unique: true,
+    },
+    contactNo: {
+      type: String,
+      required: true,
+    },
+    emergenctContactNo: {
+      type: String,
+      required: true,
+    },
+    presentAddress: {
+      type: String,
+      required: true,
+    },
+    permentAddress: {
+      type: String,
+      required: true,
+    },
+  });
+  //   res.send("Practice server running..!");
 });
 
 export default app;
