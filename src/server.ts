56e75b22e9f9ef46319471f0679decae53f4f705
NEYAZ N <neyaznafiz@outlok.com>
2023-05-12 00:17:34 +0600

    type install and impliment and app file created

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,20 @@
-const express = require("express");
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
+import app from "./app";
 
-const app = express();
-const port = 5000;
+const port : Number = 5000;
 
 // database connection
 async function main() {
   try {
     await mongoose.connect("mongodb://127.0.0.1:27017/practice-nlwd");
-  console.log("🔸 Database connection established...");
+    console.log("🔸 Database connection established...");
+
+    //   run the server
+    app.listen(port, () => {
+      console.log(`🔸 Server is listening on port ${port}`);
+    });
   } catch (err) {
-    console.error('Failed to connect Database');
+    console.error("🔻 Failed to connect Database");
   }
 }
 main();
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.listen(port, () => {
-  console.log(`🔸 Server is listening on port ${port}`);
-});
